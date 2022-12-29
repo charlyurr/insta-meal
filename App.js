3dccd7416623a9e2bc20c0e3efc8aa70f7f3b0c6
charlesrunhare <>
2022-12-29 16:00:49 +0200

    Update

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import Header from "./Shared/Header";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
+import PushNotifications from "./android/app/src/notifications/PushNotifications";
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
         <Header />
-        <Main />
+        {/* <Main /w */}
         {/* <ProductContainer /> */}
+        <PushNotifications />
       </NavigationContainer>
     </Provider>
   );
